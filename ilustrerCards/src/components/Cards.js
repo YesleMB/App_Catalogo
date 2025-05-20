386cb3e5f6df5cdc0d0e2344f01dc22a9de8b644
YesleMB <wesleymb2015@gmail.com>
2025-05-19 23:17:31 -0300

    rendereizar cards

--- ilustrerCards/src/components/Cards.js
+++ ilustrerCards/src/components/Cards.js
@@ -1,14 +1,39 @@
 import styled from 'styled-components';
 
-export const Cards = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(255, 255, 255, 0.5);
-    border-radius: 25px;
-   
-    
-`
+export const CardsConteiner = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 10rem;
+  height: 10rem;
+
+ 
+`;
+
+export const CardsImage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 10rem;
+  height: 10rem;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 25px;
+`;
+
+export const CardsDescricao = styled(CardsImage)`
+  display: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(174, 170, 170, 0.77);
+  box-shadow: 5px -8px rgb(200, 200, 200);
+  transition: all 0.5s ease-in-out;
+  z-index: 1;
+`;
+
+export const Cardlist =
+  {
+    CardC: CardsImage,
+    Descricao: CardsDescricao
+
+  }
--- ilustrerCards/src/pages/pagina1.jsx
+++ ilustrerCards/src/pages/pagina1.jsx
@@ -1,13 +1,44 @@
-import { Cards } from "../components/Cards";
+import {Cardlist,CardsImage, CardsConteiner,CardsDescricao } from "../components/Cards";
+
+const CardConteudo =[
+    {
+        id: 1,
+        card: <CardsImage>Card 1</CardsImage>,
+        descricao: <CardsDescricao>Descrição do Card 1</CardsDescricao>
+    },
+    {
+        id: 2,
+        card: <CardsImage>Card 2</CardsImage>,
+        descricao: <CardsDescricao>Descrição do Card 2</CardsDescricao>
+    },
+    {
+        id: 3,
+        card: <CardsImage>Card 3</CardsImage>,
+        descricao: <CardsDescricao>Descrição do Card 3</CardsDescricao>
+    },
+    {
+        id: 4,
+        card: <CardsImage>Card 4</CardsImage>,
+        descricao: <CardsDescricao>Descrição do Card 4</CardsDescricao>
+    }
+]
 
  const Pagina1 = () => {
     return (
         <div>
         <h1>Pagina 1</h1>
-        <p>Contenido de la página 1</p>
-        <Cards>TESTE</Cards>
-        </div>
-    );
-    }
+        <CardsConteiner>
+            {CardConteudo.map((card) => (
+                <Cardlist.CardC key={card.id}>
+                    {card.card}
+                    {card.descricao}
+                </Cardlist.CardC>
+            ))}
+        </CardsConteiner>
+
+
+ </div> 
+    )
+    };
 
     export default Pagina1;
